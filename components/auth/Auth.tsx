e40a39eefc2c3b0c19f0805386b70e5f3fbf837a
saiba <starkiller@mailbox.org>
2022-06-03 19:14:17 +0200

    form fields

--- components/auth/Auth.tsx
+++ components/auth/Auth.tsx
@@ -8,7 +8,17 @@ const Auth: FC<Props> = ({ authenticate }) => {
   return (
     <div>
       <h2>auth</h2>
-      <button onClick={authenticate}> sign in</button>
+      <form>
+        <input type="text" name="email" required placeholder="email" />
+        <input
+          type="password"
+          name="password"
+          required
+          placeholder="password"
+        />
+        <input type="checkbox" /> <a>stay signed in?</a>
+        <button onClick={authenticate}> sign in</button>
+      </form>
     </div>
   );
 };
