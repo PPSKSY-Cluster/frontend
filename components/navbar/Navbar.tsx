b8186448a0b87102bc7b587a9c630088538bc1da
bilguungantulga <bilguun@ninox.com>
2022-06-06 20:45:01 +0200

    refactor + sass config

---
+++ components/navbar/Navbar.tsx
@@ -0,0 +1,75 @@
+import { FC } from "react";
+
+interface Props {}
+
+const Navbar: FC<Props> = ({}) => {
+  return (
+    <>
+      <nav className="navbar navbar-expand-md border-bottom border-primary">
+        <div className="container-fluid">
+          <a className="navbar-brand text-primary fw-bold fs-4" href="#">
+            Cluster Thruster
+          </a>
+          <div className="d-md-none">
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarToggleExternalContent"
+              aria-controls="navbarToggleExternalContent"
+              aria-expanded="false"
+            >
+              <i
+                className="bi bi-list border border-dark rounded px-2"
+                style={style.icon}
+              ></i>
+            </button>
+          </div>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <div className="navbar-nav me-auto mb-lg-0">
+              <a className="list-group-item list-group-item-action" href="#">
+                Cluster
+              </a>
+              <a className="list-group-item list-group-item-action" href="#">
+                Options
+              </a>
+            </div>
+            <button className="btn btn-sm btn-outline-grey" type="button">
+              Abmelden
+            </button>
+          </div>
+        </div>
+      </nav>
+      <div
+        className="collapse border-bottom border-primary"
+        id="navbarToggleExternalContent"
+      >
+        <div className="">
+          <div className="list-group">
+            <button className="btn btn-sm btn-outline-grey m-3" type="button">
+              <i className="bi bi-box-arrow-in-right" />
+              &nbsp; Abmelden
+            </button>
+            <a
+              className="list-group-item list-group-item-action"
+              aria-current="page"
+              href="#"
+            >
+              Cluster
+            </a>
+            <a className="list-group-item list-group-item-action" href="#">
+              Options
+            </a>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+const style = {
+  icon: {
+    fontSize: "2rem",
+  },
+};
+
+export default Navbar;
