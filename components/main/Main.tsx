d628141d5ac822c018898f4979612234ca8de88b
vladischul <104712955+vladischul@users.noreply.github.com>
2022-06-17 14:57:43 +0200

    Update Main.tsx

--- components/main/Main.tsx
+++ components/main/Main.tsx
@@ -1,10 +1,6 @@
 import { FC } from "react";
 import { MobileMenu } from "types/MobileMenu";
 import Navbar from "../navbar/Navbar";
-import User from "../user/User";
-import Options from "../user/Options"
-import {useState} from "react";
-import { setHttpAgentOptions } from "node_modules/next/dist/server/config";
 
 interface Props {
   children?: any;
