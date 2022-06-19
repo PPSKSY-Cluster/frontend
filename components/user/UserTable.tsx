46b0ae4c8bda3614c2a21f2edd2e2a4d21567ec8
vladischul <104712955+vladischul@users.noreply.github.com>
2022-06-19 13:38:35 +0200

    Update UserTable.tsx

--- components/user/UserTable.tsx
+++ components/user/UserTable.tsx
@@ -10,33 +10,7 @@ import UserUpdate from "./UserUpdate";
   const UserTable =  () => {
     var searchName;
     const [users, setUser] = useState([]);
-
-    /*useEffect(() => {
-      async function getUsers(){
-        setUsers(await getAllUsers())
-      } 
-      getUsers();
-    },[])
-
     
-    const getAllUsers = async () => {
-      const response = await axios.get("http://localhost:8080/api/users", {headers:{'Content-Type': 'application/json',
-      'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhZG1pbiI6dHJ1ZSwiZXhwIjoxNjU1MTI4MzM2LCJ1c2VybmFtZSI6ImZvbyJ9.RmKj9VTsxjMR1JnMHaE4bO6wbMF8tCoGt7e3EterseU'}
-    })
-        return response.data
-    }
-
-    const getUser = async () => {
-      var user: IUser;
-      //var searchName = (document.getElementById("searchName") as HTMLInputElement).value;
-      const response = await axios.get("http://localhost:8080/api/users" + "/62a5ef5f9b388bcfc925fcdc", {headers:{'Content-Type': 'application/json',
-      'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhZG1pbiI6dHJ1ZSwiZXhwIjoxNjU1MTI4MzM2LCJ1c2VybmFtZSI6ImZvbyJ9.RmKj9VTsxjMR1JnMHaE4bO6wbMF8tCoGt7e3EterseU'}
-    })
-    }
-
-    console.log(getAllUsers())
-    */
-
     const [currentItem, setCurrentItem] = useState<IUser>({
       username: "",
       _id:0,
@@ -76,89 +50,42 @@ import UserUpdate from "./UserUpdate";
     };
 
     return (
-      <>
-        <table className="table table-hover m-3">
-          <thead>
-            <tr>
-              <th>Nr</th>
-              <th className="text-center">Name</th>
-              <th className="text-center">Beschreibung</th>
-              <th className="text-center">Bearbeiten</th>
-              <th className="text-center">Löschen</th>
-            </tr>
-          </thead>
-          <tbody>
-            {users?.map((userItem, index) => {
-              return (
-                <UserItem
-                  key={userItem._id}
-                  userItem={userItem}
-                  count={++index}
-                  setCurrentItem={setCurrentItem}
-                ></UserItem>
-              );
-            })}
-          </tbody>
-        </table>
-  
-        <ConfirmDialog
-          id={"userDeletion"}
-          accept={{ caption: "Löschen", onClick: onDeleteClick }}
-          title={"User Löschen"}
-          text={`Möchten Sie ${currentItem?.username} wirklich löschen?`}
-        />
-        <UserUpdate
-          currentItem={currentItem}
-          setCurrentItem={setCurrentItem}
-          onSubmit={onUpdateClick}
-        />
-      </>
+      <table className="table table-hover m-3">
+        <thead>
+          <tr>
+            <th>Nr</th>
+            <th className="text-center">Name</th>
+            <th className="text-center">Beschreibung</th>
+            <th className="text-center">Bearbeiten</th>
+            <th className="text-center">Löschen</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users?.map((userItem, index) => {
+            return (
+              <UserItem
+                key={userItem._id}
+                userItem={userItem}
+                count={++index}
+                setCurrentItem={setCurrentItem}
+              ></UserItem>
+            );
+          })}
+        </tbody>
+      </table>
+
+      <ConfirmDialog
+        id={"userDeletion"}
+        accept={{ caption: "Löschen", onClick: onDeleteClick }}
+        title={"User Löschen"}
+        text={`Möchten Sie ${currentItem?.username} wirklich löschen?`}
+      />
+      <UserUpdate
+        currentItem={currentItem}
+        setCurrentItem={setCurrentItem}
+        onSubmit={onUpdateClick}
+      />
     );
   };
-    /*
-    return (
-      <div className="d-flex flex-row">
-        <Sidebar
-          title={"User"}
-          elements={["User suchen", "User erstellen"]}
-        />
-        <table className="table m-3">
-          <thead>
-          <h2>User suchen  </h2>
-          <div>
-            <div>
-              <input 
-                id = "searchName"
-                type = "text"
-                className ="form-control"
-                placeholder="username"
-                value = {searchName}
-              ></input>
-            </div>
-            <div className ="pull-left">
-              <button type ="button">Suchen</button>
-            </div>
-          </div>
-            <tr>
-              <th>Nr</th>
-              <th>Name</th>
-              <th>Rolle</th>
-              <th>Edit</th>
-            </tr>
-          </thead>
-          <tbody>
-            {users?.map((userItem, index) => {
-              return (
-                <UserItem
-                  key={userItem._id}
-                  userItem={userItem}
-                  count={++index}
-                ></UserItem>
-              );
-            })}
-          </tbody>
-        </table>
-      </div>
-    );
-  };*/
+
 export default UserTable;
