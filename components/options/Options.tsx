c04ee868e824b8cadd565b58e0a0e494c8c24527
vladischul <104712955+vladischul@users.noreply.github.com>
2022-06-19 13:31:41 +0200

    Update Options.tsx

--- components/options/Options.tsx
+++ components/options/Options.tsx
@@ -19,34 +19,6 @@ import OptionsAPI from "api/options";
         _id:0,
       });
 
-    /*useEffect(() => {
-      async function getUsers(){
-        setUsers(await getUser())
-      } 
-      getUsers();
-    },[])
-
-    const editUser = async () => {
-        const response = await axios.put("http://localhost:8080/api/users/62a6f3a85e59b4912e234457", {username:nameRef.current.value.trim()}, {headers:{'Content-Type': 'application/json',
-        'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhZG1pbiI6dHJ1ZSwiZXhwIjoxNjU1MTI4MzM2LCJ1c2VybmFtZSI6ImZvbyJ9.RmKj9VTsxjMR1JnMHaE4bO6wbMF8tCoGt7e3EterseU'}
-        })
-    }
-    
-    const getUser = async () => {
-        const response = await axios.get("http://localhost:8080/api/users/62a6f3a85e59b4912e234457", {headers:{'Content-Type': 'application/json',
-        'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhZG1pbiI6dHJ1ZSwiZXhwIjoxNjU1MTI4MzM2LCJ1c2VybmFtZSI6ImZvbyJ9.RmKj9VTsxjMR1JnMHaE4bO6wbMF8tCoGt7e3EterseU'}
-        })
-        return response.data
-    }
-
-    const deleteUser = async () => {
-        const response = await axios.delete("http://localhost:8080/api/users/62a5ef5f9b388bcfc925fcdc", {headers:{'Content-Type': 'application/json',
-        'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhZG1pbiI6dHJ1ZSwiZXhwIjoxNjU1MTI4MzM2LCJ1c2VybmFtZSI6ImZvbyJ9.RmKj9VTsxjMR1JnMHaE4bO6wbMF8tCoGt7e3EterseU'}
-        })
-    }
-
-    console.log(getUser());*/
-
     const onDeleteClick = async () => {
         try {
           const response = await OptionsAPI.delete(currentItem._id);
